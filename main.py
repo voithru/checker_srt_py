1307190ae631f4c6d4dde925a586b0fcebfb53f5
hyunjin717 <qc.manager@voithru.com>
2024-09-12 17:24:12 +0900

    modify-main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import tkinter as tk
 from application import Application
 import sys
-import os
+from pathlib import Path
 
-if getattr(sys, 'frozen', False):
-    application_path = sys._MEIPASS
-else:
-    application_path = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(application_path)
+def setup_environment():
+    if getattr(sys, 'frozen', False):
+        # PyInstaller로 빌드된 실행 파일인 경우
+        application_path = Path(sys._MEIPASS)
+    else:
+        # 스크립트로 실행되는 경우
+        application_path = Path(__file__).parent
+    
+    # 애플리케이션 경로를 시스템 경로에 추가
+    sys.path.append(str(application_path))
 
 def main():
+    setup_environment()
+    
     root = tk.Tk()
     app = Application(master=root)
     root.protocol("WM_DELETE_WINDOW", app.on_closing)
